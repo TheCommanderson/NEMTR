fc6445c8ec993da0cc230790db040697e0218b48
Ubuntu <ubuntu@ip-172-31-34-132.ec2.internal>
2020-03-02 04:30:07 +0000

    login redirection

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :admins
   resources :drivers
   resources :patients
+  resources :sessions
+  root 'sessions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
