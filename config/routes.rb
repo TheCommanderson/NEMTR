eabe1aab4c41da2732c00f32b6fabecf01293b92
Xin Xiao <cindy_xiao98@yahoo.ca>
2020-03-02 15:46:10 +0000

    linked registration to 'new' for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#index'
   resources :appointments
   resources :drivers
   resources :patients
   resources :sessions
-  root 'sessions#index'
   resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
