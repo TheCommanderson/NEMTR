4eb04c9d889e7b57b572db755140cd46b4f56174
spencer <spencerc.anderson@tamu.edu>
2020-03-19 16:03:11 +0000

    began work on sessions, starting with the sessions controller and some application_controller helper functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    helper_method :logged_in?
+    
+    # Should return the current user or nil if not logged in
+    def current_user
+        case session[:login_type]
+        when "P"
+            _user = Patient.find(id: session[:user_id])
+        when "D"
+            _user = Driver.find(id: session[:user_id])
+        when "A"
+            _user = Admin.find(id: session[:user_id])
+        else
+            _user = nil
+        end
+    end
+    
+    def logged_in?
+        !session[:login_type].nil?
+    end
 end
