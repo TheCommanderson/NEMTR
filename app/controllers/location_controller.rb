e3a1b6e345e71c4dee0326a9af981fdb44ca5168
Jason Lien <jasonlien0312@gmail.com>
2020-04-02 22:39:42 -0500

    working on edit page of location

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,4 +1,4 @@
-class LocationsController < ApplicationController
+class LocationController < ApplicationController
   before_action :set_appointment
     
   def new
@@ -12,11 +12,24 @@ class LocationsController < ApplicationController
     @appointment.save
   end
   
+  def edit
+    @patient = Patient.find(params[:patient_id])
+  end
+
   def update
   end
 
   private
   def set_appointment
-    @appointment = Appointment.find(params[:id])
+    # @appointment = Appointment.find(params[:id])
+  end
+  def location_params
+    params.require(:location).permit(
+      :name, 
+      :addr1, 
+      :addr2, 
+      :city, 
+      :state, 
+      :zip)
   end
 end
