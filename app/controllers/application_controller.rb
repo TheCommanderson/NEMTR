4658b5322e1ec825a48e9bc305c1f35a2ad66e16
spencer <spencerc.anderson@tamu.edu>
2020-04-07 23:02:00 +0000

    started work on the matching algorithm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    require 'date'
+    
     helper_method :current_user
     helper_method :logged_in?
     helper_method :sign
@@ -33,5 +35,39 @@ class ApplicationController < ActionController::Base
     
     def sign(x)
         "++-"[x <=> 0]
+    end
+    
+    def valid_drivers(appt)
+        dt_format = '%Y-%m-%d %k:%l:%M'
+        appt_start_time = DateTime.strptime(appt.datetime, dt_format)
+        appt_end_time = (appt_start_time.to_time + appt.est_time.minutes).to_datetime
+        drivers = []
+        Driver.all.each do |driver|
+            @driver_apps = Appointment.where(driver_id: driver[:id])
+            driver_today_sch = driver.schedule.where(current: true).first[Time.now.strftime("%A")]
+            
+        end
+    end
+    
+    def matching_alg
+       @matchable_apps = Appointment.where(status: -1)
+       if !@matchable_apps.exists?
+           return
+       end
+       @matchable_apps.each do |appt|
+          poss_drivers = valid_drivers(appt)
+          len = poss_drivers.to_a.length
+          if len == 0
+              # HANDLE FAILURE
+              next
+          elsif len > 1
+              rand_int = rand(len)
+              driver = poss_drivers[rand_int]
+          else
+              driver = poss_drivers.first
+          end
+          new_atts = {status: 1, driver_id: driver[:id]}
+          appt.update_attributes(new_atts)
+       end
     end
 end
