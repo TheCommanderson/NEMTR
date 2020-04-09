969ce397a0bf8e8f0cbad4885cee90c401d3bf5e
sheldontheking <wei980622@gmail.com>
2020-04-08 19:08:09 -0500

    Revert "adding gems and fixing models"
    This reverts commit f821b5f286c7c6aa2d61b1955c3104eca62ac518.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,5 @@
 class Location
   include Mongoid::Document
-  include Mongoid::Geospatial
-
-  field :location, type: Point
-
   field :addr1, type: String
   field :addr2, type: String
   field :city, type: String
