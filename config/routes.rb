b72fa468d444937115f526189287e169a0c6613c
Jason Lien <jasonlien0312@gmail.com>
2020-04-09 22:56:14 -0500

    Done with 4 action buttons (update*3 and cancel)

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   root 'sessions#index'
-  resources :appointments
+  resources :appointments do
+    resources :location
+  end
     get 'drivers', to: 'drivers#pending'
     get 'drivers_home', to: 'drivers#index'
+    post '/appointments/:appointment_id/location/:id/edit', to: 'location#update'
   resources :drivers
     get 'patients', to:'patients#pending'
     get 'patients_home', to: 'patients#index'
-  resources :patients do
-    resources :location
-  end
+  resources :patients 
   resources :sessions
   resources :admins
+  resources :locations
 
   get 'location/:id/edit', to: 'locations#edit'
   patch 'location/:id/edit', to: 'locations#update'
