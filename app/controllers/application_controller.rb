13c1f0e3109b311a7bca8cf4886c217564631781
spencer <spencerc.anderson@tamu.edu>
2020-04-15 18:41:19 +0000

    texting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,14 @@ class ApplicationController < ActionController::Base
     before_action :authorized
     # Should return the current user or nil if not logged in
     def current_user
+        _user = nil        
         case session[:login_type]
         when "P"
-            _user = Patient.find(session[:user_id])
+            _user = Patient.find(session[:user_id]) rescue nil
         when "D"
-            _user = Driver.find(session[:user_id])
+            _user = Driver.find(session[:user_id]) rescue nil
         when "A"
-            _user = Admin.find(session[:user_id])
+            _user = Admin.find(session[:user_id]) rescue nil
         else
             _user = nil
         end
@@ -52,8 +53,11 @@ class ApplicationController < ActionController::Base
     end
     
     def valid_drivers(appt, cur)
+        @dr_debug_log = ['start']
         appt_start_time = DateTime.strptime(appt.datetime, dt_format).to_time.strftime("%H%M").to_i
         appt_end_time = (DateTime.strptime(appt.datetime, dt_format).to_time + appt.est_time.minutes).strftime("%H%M").to_i #est_time in minutes?
+        @dr_debug_log.append('appt start time: ' + appt_start_time)
+        @dr_debug_log.append('appt end time: ' + appt_end_time)
         drivers = []
         Driver.all.each do |driver|
             
@@ -64,61 +68,73 @@ class ApplicationController < ActionController::Base
                 next
             end
             
-            # Check if the driver has any conflicts with appointments
-            @driver_apps = Appointment.where(driver_id: driver[:id])
-            @bad = nil
-            @driver_apps.each do |dr_appt|
-                @bad = false
-                dr_appt_start_time = DateTime.strptime(dr_appt.datetime, dt_format).to_time.strftime("%H%M").to_i
-                dr_appt_end_time = (DateTime.strptime(dr_appt.datetime, dt_format).to_time + dr_appt.est_time.minutes).strftime("%H%M").to_i
-                if sign(appt_start_time-dr_appt_start_time) != sign(appt_start_time-dr_appt_end_time) # appt starts in the middle of conlficting appt
-                    @bad = true
-                    break
-                elsif sign(appt_end_time-dr_appt_start_time) != sign(appt_end_time-dr_appt_end_time) # appt ends in the middle of conflicting appt
-                    @bad = true
-                    break
-                elsif appt_start_time <= dr_appt_start_time && appt_end_time >= dr_appt_end_time # conflict is contained completely inside appt
-                    @bad = true
-                    break
-                # NOTE: if appt is contained completely in the conflict then it will execute the first if statement
-                end
-            end
-            if @bad
-               next 
-            end
             
             # Checking if the appointment falls within the range of thier schedule
             driver_today_sch = driver.schedule.where(current: cur).first[Time.now.strftime("%A")]
             driver_today_time_start = driver_today_sch[0..3].to_i
             driver_today_time_end = driver_today_sch[5..8].to_i
             
-            if !(driver_today_time_start <= appt_start_time && appt_start_time <= driver_today_time_end)
+            if !(appt_start_time < driver_today_time_start ||
+                    appt_end_time < driver_today_time_start ||
+                    appt_start_time > driver_today_time_end ||
+                    appt_end_time > driver_today_time_end
+                )
                 next
-            elsif !(driver_today_time_start <= appt_end_time && appt_end_time <= driver_today_time_end)
-                next
+            end
+            
+            # Check if the driver has any conflicts with appointments
+            @driver_apps = Appointment.where(driver_id: driver[:id])
+            @bad = nil
+            @driver_apps.each do |conflict|
+                @bad = false
+                conflict_start_time = DateTime.strptime(conflict.datetime, dt_format).to_time.strftime("%H%M").to_i
+                conflict_end_time = (DateTime.strptime(conflict.datetime, dt_format).to_time + conflict.est_time.minutes).strftime("%H%M").to_i
+                if sign(appt_start_time-conflict_start_time) != sign(appt_start_time-conflict_end_time) # appt starts in the middle of conlficting appt
+                    @bad = true
+                    break
+                elsif sign(appt_end_time-conflict_start_time) != sign(appt_end_time-conflict_end_time) # appt ends in the middle of conflicting appt
+                    @bad = true
+                    break
+                elsif appt_start_time <= conflict_start_time && appt_end_time >= conflict_end_time # conflict is contained completely inside appt
+                    @bad = true
+                    break
+                # NOTE: if appt is contained completely in the conflict then it will execute the first if statement
+                end
+            end
+            # found a conflict, move to next driver
+            if @bad
+               next 
             end
             
             drivers.push(driver)
         end
+        drivers
     end
     
     def matching_alg
+        @debug_log = ['start']
+        
         this_monday = getMonday(DateTime.now)
         next_monday = getMonday((Time.now + 7.days).to_datetime)
+        @debug_log.append('this monday: ' + this_monday.to_s)
+        @debug_log.append('next monday: ' + next_monday.to_s)
         @matchable_apps = Appointment.where(status: 0)
         if !@matchable_apps.exists?
+            @debug_log.append('no valid appointments to match')
             return
         end
 
         @matchable_apps.each do |appt|
+            @debug_log.append('appointment id: ' + appt.id.toString)
             if ![this_monday.to_s[0..10], next_monday.to_s[0..10]].include? getMonday(DateTime.strptime(appt.datetime, dt_format)).to_s[0..10]
+                @debug_log.append('appointment is not within the next 2 weeks')
                 next
             end
             # TESTED TO HERE
             cur = getMonday(DateTime.strptime(appt.datetime, dt_format)) == this_monday
             poss_drivers = valid_drivers(appt, cur)
-            str = poss_drivers
             len = poss_drivers.to_a.length
+            @debug_log.append(len.to_s + ' drivers found')
             if len == 0
                 # HANDLE FAILURE
                 next
@@ -128,6 +144,7 @@ class ApplicationController < ActionController::Base
             else
                 driver = poss_drivers.first
             end
+            @debug_log.append('appt assigned to ' + driver.first_name)
             new_atts = {status: 1, driver_id: driver[:id]}
             appt.update_attributes(new_atts)
         end
