ecb3d30ae7f09ff36c40b9b90e523ea5b1cf69a6
spencer <spencerc.anderson@hotmail.com>
2020-05-13 13:30:38 -0500

    Changed the driver schedule index page to look like it was actually designed by a human and not like a cat ran accross my keyboard.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,7 +5,10 @@ class SchedulesController < ApplicationController
     @next_schedule = @driver.schedule.where(:current => false).first
     @current_sch_readable = {}
     @next_sch_readable = {}
-    
+    @days_of_this_week = getDatesOfWeek(getMonday(DateTime.now))
+    @days_of_next_week = getDatesOfWeek(getMonday((Time.current + 7.days).to_datetime))
+
+    # Get the readable format of the current schedule
     @current_schedule.attributes.each do |name, val|
       next if not days.include? name
       if val[0..3] == val[5..8]
@@ -15,7 +18,8 @@ class SchedulesController < ApplicationController
         @current_sch_readable[name] = day_str
       end
     end
-    
+
+    # Get the readable format of the next schedule
     @next_schedule.attributes.each do |name, val|
       next if not days.include? name
       if val[0..3] == val[5..8]
@@ -26,18 +30,18 @@ class SchedulesController < ApplicationController
       end
     end
   end
-  
+
   def new
     @driver = Driver.find(params[:driver_id])
     @schedule = @driver.schedule.new
   end
-  
+
   def create
-    @driver = Driver.find(params[:driver_id]) 
+    @driver = Driver.find(params[:driver_id])
     @schedule = @driver.schedule.build(params[:schedule])
     @driver.save
   end
-  
+
   def edit
     @driver = Driver.find(params[:driver_id])
     @schedule = @driver.schedule.where(:id => params[:id]).first
@@ -52,7 +56,7 @@ class SchedulesController < ApplicationController
       @default_vals[name] = [val[0..1], val[2..3], val[5..6], val[7..8]]
     end
   end
-  
+
   def update
     @driver = Driver.find(params[:driver_id])
     @schedule = @driver.schedule.where(:id => params[:id]).first
@@ -68,17 +72,22 @@ class SchedulesController < ApplicationController
       end
       new_sch[day] = day1 + ' ' + day2
     end
-    
+
     @schedule.update_attributes(new_sch)
     @debug_log = matching_alg
     redirect_to drivers_home_url
   end
-  
+
   private
   def schedule_params
     params.require(:schedule).permit(:Monday, :Tuesday, :Wednesday, :Thursday, :Friday, :Saturday, :Sunday)
   end
   def days
     _days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  end
+  def getDatesOfWeek(monday)
+    _dates = []
+    (0..6).each {|i| _dates.append((monday.to_time + i.days).strftime("%B %-d"))}
+    _dates
   end
 end
