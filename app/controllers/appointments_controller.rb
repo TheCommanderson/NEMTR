5d64b5cb163baadcb8ec469857c2b5d611bfad0a
spencer <spencerc.anderson@hotmail.com>
2020-05-19 17:17:11 -0500

    Patients now only have the option to choose one destination from a predetermined list of destinations given to them from their admin for appointments.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -17,6 +17,7 @@ class AppointmentsController < ApplicationController
   # GET /appointments/new
   def new
     @appointment = Appointment.new
+    @patient = Patient.find(params[:patient_id])
   end
 
   # GET /appointments/1/edit
@@ -27,8 +28,17 @@ class AppointmentsController < ApplicationController
   # POST /appointments
   # POST /appointments.json
   def create
+    patient = Patient.find(params[:appointment][:patient_id])
+    loc = patient.preset.find(params[:loc_select])
+    loc_hash = {name: loc.name, addr1: loc.addr1, addr2: loc.addr2, city: loc.city, state: loc.state, zip: loc.zip}
     @appt_parms = appointment_params
     @appt_parms['datetime'] =params[:appointment]['dt(1i)'] + '-' + params[:appointment]['dt(2i)'] + '-' + params[:appointment]['dt(3i)'] + ' ' + params[:appointment]['dt(4i)'] + ':' + params[:appointment]['dt(5i)']
+    if @appt_parms[:location_attributes]['0'][:name] == 'tmp'
+      @appt_parms[:location_attributes]['0'].merge! loc_hash
+    else
+      @appt_parms[:location_attributes]['1'].merge! loc_hash
+    end
+
     @appointment = Appointment.new(@appt_parms)
     respond_to do |format|
       if @appointment.save
@@ -75,7 +85,7 @@ class AppointmentsController < ApplicationController
   def destroy
     @appointment.destroy
     respond_to do |format|
-      format.html { redirect_to "/patients_home", notice: 'Appointment was successfully destroyed.' }
+      format.html { redirect_to "/patients_home", notice: 'Appointment was successfully deleted.' }
       format.json { head :no_content }
     end
   end
