9789fe8e0c41e4889159ea1e7aceeef9e7d3bd76
spencer <spencerc.anderson@hotmail.com>
2020-06-26 15:55:16 -0500

    correctly made stats model comments woops

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
-'''
-class Stat
-  include Mongoid::Document
-  field :rides, type: Integer
-  field :errors, type: Integer
-  field :current, type: Boolean
-  field :date, type: String
-  field :drivers, type: Integer
-  field :patients, type: Integer
-end
-
-def self.collect
-  Stat.find_by(current: true) do |s|
-    s.patients = Patient.count
-    s.drivers = Driver.count
-    s.current = false
-  end
-  paramss = { rides: 0, errors: 0, current: true, date: Date.today.strftime('%B %Y'), drivers: 0, patients: 0 }
-  next_stat = Stat.new(params)
-end
-'''
+# class Stat
+#   include Mongoid::Document
+#   field :rides, type: Integer
+#   field :errors, type: Integer
+#   field :current, type: Boolean
+#   field :date, type: String
+#   field :drivers, type: Integer
+#   field :patients, type: Integer
+# end
+#
+# def self.collect
+#   Stat.find_by(current: true) do |s|
+#     s.patients = Patient.count
+#     s.drivers = Driver.count
+#     s.current = false
+#   end
+#   paramss = { rides: 0, errors: 0, current: true, date: Date.today.strftime('%B %Y'), drivers: 0, patients: 0 }
+#   next_stat = Stat.new(params)
+# end
