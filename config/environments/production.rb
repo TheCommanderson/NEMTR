7f69c3c7de6ea6fbfc87e306f799e8624b0f7303
spencer <spencerc.anderson@hotmail.com>
2020-06-26 15:40:34 -0500

    changed mail to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mail.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: Rails.application.credentials.gmail_username,
