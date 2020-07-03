7cf259f80b3ef046f3c9d180ca82c3895ca65d84
spencer <spencerc.anderson@hotmail.com>
2020-07-02 21:51:49 -0500

    Changed the schedued server jobs to perform their tasks at 12am on sunday night/monday morning instead of saturday night/sunday morning.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Use this file to easily define all of your cron jobs.
 set :output, 'log/cron.log'
 # every 2.hours do
@@ -9,15 +11,16 @@ set :output, 'log/cron.log'
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
-every :sunday, at: "12:01 am" do
-    runner "Schedule.rollover"
-end
-every :sunday, at: "12:01 am" do
-    runner "Driver.blacklist_reset"
+every :monday, at: '12:01 am' do
+  runner 'Schedule.rollover'
 end
 
-every 1.day, at: "12:01 am" do
-    runner "Appointment.clean_past_appointments"
+every :monday, at: '12:01 am' do
+  runner 'Driver.blacklist_reset'
+end
+
+every :monday, at: '12:01 am' do
+  runner 'Appointment.clean_past_appointments'
 end
 
 # Learn more: http://github.com/javan/whenever
