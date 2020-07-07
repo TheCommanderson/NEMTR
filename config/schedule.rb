72a0eac2709657494ffcab6a7690c465e618ea31
spencer <spencerc.anderson@hotmail.com>
2020-07-07 17:30:05 -0500

    changed the timing on the scheduled stuff

--- config/schedule.rb
+++ config/schedule.rb
@@ -15,11 +15,11 @@ every :monday, at: '12:01 am' do
   runner 'Schedule.rollover'
 end
 
-every :monday, at: '12:01 am' do
+every :monday, at: '12:05 am' do
   runner 'Driver.blacklist_reset'
 end
 
-every :monday, at: '12:01 am' do
+every :monday, at: '12:10 am' do
   runner 'Appointment.clean_past_appointments'
 end
 
