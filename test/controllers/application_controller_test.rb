46accd0844aa584806185b8584c9621f99bc62e7
spencer <spencerc.anderson@hotmail.com>
2021-10-23 15:34:40 -0500

    clean up time

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'logged_in returns false when user is not logged in' do
+  test 'test_logged_in_returns_false_for_logged_out_user' do
     assert true
   end
 end
