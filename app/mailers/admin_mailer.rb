3b3ae6b9c6fc287037cb8a34bfb35429fe80217f
spencer <spencerc.anderson@hotmail.com>
2021-10-25 19:34:06 -0500

    Mailers!

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class AdminMailer < ApplicationMailer
+  default from: 'ride2health@uwbv.org'
+
+  def new_patient_email
+    @patient = params[:patient]
+    emails = User.where(host_org: @patient.host_org, _type: 'Healthcareadmin').collect(&:email).join(',')
+
+    mail(to: @admin.email, subject: 'New Ride2Health Patient')
+  end
+
+  def new_driver_email
+    @driver = params[:driver]
+    emails = User.where(_type: 'Sysadmin').collect(&:email).join(',')
+
+    mail(to: emails, subject: 'New Ride2Health Driver')
+  end
+
+  def new_admin_email
+    @admin = params[:admin]
+    emails = User.where(_type: 'Sysadmin').collect(&:email).join(',')
+
+    mail(to: emails, subject: 'New Ride2Health Admin')
+  end
+
+  def short_cancel_email
+    @driver = params[:driver]
+    @patient = params[:patient]
+    emails = User.where(_type: 'Sysadmin').collect(&:email).join(',')
+
+    mail(to: emails, subject: 'Ride2Health Driver Canceled an imminent trip!')
+  end
+
+  def issue_email
+    @reporter = params[:reporter]
+    @driver = params[:driver]
+    @patient = params[:patient]
+    @issue = params[:issue]
+    emails = User.where(_type: 'Sysadmin').collect(&:email).join(',')
+
+    mail(to: emails, subject: 'Issue reported for a recent Ride2Health trip')
+  end
+
+  def password_reset_email
+    @password = params[:password]
+    mail(to: params[:user_email], subject: 'Ride2Health password reset')
+  end
+end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class UserMailer < ApplicationMailer
+  default from: 'ride2health@uwbv.org'
+
+  def ride_assigned_email
+    @appointment = params[:appt]
+    @driver = User.find(@appointment.driver_id)
+    mail(to: @driver.email, subject: 'New Ride2Health Assignment')
+  end
+
+  def ride_created_email
+    @appointment = params[:appt]
+    @patient = User.find(@appointment.patient_id)
+    mail(to: @patient.email, subject: 'Ride2Health Trip Confirmed')
+  end
+
+  def ride_updated_email
+    @appointment = params[:appt]
+    @patient = User.find(@appointment.patient_id)
+    @driver = User.find(@appointment.driver_id)
+    mail(to: @patient.email, subject: 'Ride2Health Trip Updated')
+  end
+end
