b1e78c651ccae0ea923a73efdbb7465a5eddccb8
spencer <spencerc.anderson@hotmail.com>
2021-12-17 18:25:24 -0600

    Change the API key for Google APIs to be the new ride2health project.

--- app/frontend/packs/appointment.js
+++ app/frontend/packs/appointment.js
@@ -1,42 +1,5 @@
 import Jquery from 'jquery'
 
-function initMap(start, dest) {
-  // make sure the coords is received!
-  // console.log(start)
-  // console.log(dest)
-
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRenderer = new google.maps.DirectionsRenderer();
-  var pos1 = new google.maps.LatLng(start[0], start[1]);
-  var pos2 = new google.maps.LatLng(dest[0], dest[1]);
-  var mapOptions = {
-    zoom: 14,
-    center: pos1
-  }
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  directionsRenderer.setMap(map);
-  directionsRenderer.setPanel(document.getElementById('right-panel'));
-  calculateAndDisplayRoute(directionsService, directionsRenderer, pos1, pos2);
-}
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer, start, end) {
-  directionsService.route({
-    origin: start,
-    destination: end,
-    travelMode: 'DRIVING'
-  }, function(response, status) {
-    if (status === 'OK') {
-      // console.log(response)
-      // this is duration in seconds
-      console.log(response.routes[0].legs[0].duration.value)
-      var est = response.routes[0].legs[0].duration.value
-      directionsRenderer.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-}
-
 function toggleView(sel){
     if (sel == 1){
       document.getElementById('show-dest-restricted').style.display = 'block';
